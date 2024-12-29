f0254a649e64061f6e2d6083fbf838df91e2a885
Muhammad Uzair Abbas <110011934+MuhammadUzairAbbas@users.noreply.github.com>
2024-12-29 13:53:47 +0500

    chore:imgchanges

--- src/App.jsx
+++ src/App.jsx
@@ -25,17 +25,18 @@ const App = () => {
     };
 
     return (
-        <div className="p-4  mx-auto ">
+        <div className="p-4 mx-auto ">
             <Header />
 
-            <div className=" bg-custom-radial-gradient rounded-xl shadow-lg p-6  border-t-6 border-t-gray-300">
-                <div className="flex flex-col gap-4   ">
-                    <h2 className="  text-xl md:text-2xl text-center font-bold">
+            <div className="p-6 shadow-lg bg-custom-radial-gradient rounded-xl border-t-6 border-t-gray-300">
+                <div className="flex flex-col gap-4 ">
+                    <h2 className="text-xl font-bold text-center md:text-2xl">
                         Select Crop and Recommendation Type
                     </h2>
-                    <div className="mb-10 md:mb-0 flex flex-col md:flex-row mt-4 w-full justify-center gap-4">
+                    <div className="flex flex-col justify-center w-full gap-4 mt-4 mb-10 md:mb-0 md:flex-row">
+                        {/* <h3>hhhh</h3> */}
                         <select
-                            className="p-2 pl-4 border border-gray-300  rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500"
+                            className="p-2 pl-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500"
                             value={category}
                             onChange={handleCategoryChange}
                         >
@@ -66,7 +67,7 @@ const App = () => {
                                 "https://nwfc.pmd.gov.pk/new/daily-forecast-en.php"
                             }
                             target="_blank"
-                            className="bg-green-500 text-white text-center px-4 py-2 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+                            className="px-4 py-2 text-center text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
                         >
                             Get Weather
                         </Link>
@@ -83,12 +84,12 @@ const App = () => {
                             return (
                                 <div key={sub} className="mb-8">
                                     <div className="flex justify-between mb-4">
-                                        <h3 className="text-2xl font-semibold mb-4">
+                                        <h3 className="mb-4 text-2xl font-semibold">
                                             {sub}
                                         </h3>
 
                                         <button
-                                            className="bg-green-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+                                            className="px-4 py-2 text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
                                             onClick={() => {
                                                 alert("Coming Soon");
                                             }}
@@ -96,75 +97,75 @@ const App = () => {
                                             View Price
                                         </button>
                                     </div>
-                                    <table className="min-w-full bg-white text-black text border border-gray-300 rounded-lg overflow-hidden">
+                                    <table className="min-w-full overflow-hidden text-black bg-white border border-gray-300 rounded-lg text">
                                         <thead className="bg-gray-350">
                                             <tr>
-                                                <th className="px-4 py-2 border-b border-l text-left">
+                                                <th className="px-4 py-2 text-left border-b border-l">
                                                     ID
                                                 </th>
-                                                <th className="px-4 py-2 border-b text-left">
+                                                <th className="px-4 py-2 text-left border-b">
                                                     Properties
                                                 </th>
-                                                <th className="px-4 py-2 border-b text-center">
+                                                <th className="px-4 py-2 text-center border-b">
                                                     Values
                                                 </th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <tr>
-                                                <td className="px-4 py-2 border-b font-medium">
+                                                <td className="px-4 py-2 font-medium border-b">
                                                     1
                                                 </td>
-                                                <td className="px-4 py-2 border-b font-medium">
+                                                <td className="px-4 py-2 font-medium border-b">
                                                     Soil Type
                                                 </td>
-                                                <td className="px-4 py-2 border-b text-center">
+                                                <td className="px-4 py-2 text-center border-b">
                                                     {soil.soil_type}
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <td className="px-4 py-2 border-b font-medium">
+                                                <td className="px-4 py-2 font-medium border-b">
                                                     2
                                                 </td>
-                                                <td className="px-4 py-2 border-b font-medium">
+                                                <td className="px-4 py-2 font-medium border-b">
                                                     N
                                                 </td>
-                                                <td className="px-4 py-2 border-b text-center">
+                                                <td className="px-4 py-2 text-center border-b">
                                                     {soil.N}
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <td className="px-4 py-2 border-b font-medium">
+                                                <td className="px-4 py-2 font-medium border-b">
                                                     3
                                                 </td>
-                                                <td className="px-4 py-2 border-b font-medium">
+                                                <td className="px-4 py-2 font-medium border-b">
                                                     P2O5
                                                 </td>
-                                                <td className="px-4 py-2 border-b text-center">
+                                                <td className="px-4 py-2 text-center border-b">
                                                     {soil.P2O5}
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <td className="px-4 py-2 border-b font-medium">
+                                                <td className="px-4 py-2 font-medium border-b">
                                                     4
                                                 </td>
-                                                <td className="px-4 py-2 border-b font-medium">
+                                                <td className="px-4 py-2 font-medium border-b">
                                                     K2O
                                                 </td>
-                                                <td className="px-4 py-2 border-b text-center">
+                                                <td className="px-4 py-2 text-center border-b">
                                                     {soil.K2O}
                                                 </td>
                                             </tr>
                                             {Object.entries(recommendation).map(
                                                 ([key, value], index) => (
                                                     <tr key={key}>
-                                                        <td className="px-4 py-2 border-b font-medium">
+                                                        <td className="px-4 py-2 font-medium border-b">
                                                             {index + 5}
                                                         </td>
-                                                        <td className="px-4 py-2 border-b font-medium">
+                                                        <td className="px-4 py-2 font-medium border-b">
                                                             {key}
                                                         </td>
-                                                        <td className="px-4 py-2 border-b text-center">
+                                                        <td className="px-4 py-2 text-center border-b">
                                                             {value}
                                                         </td>
                                                     </tr>
@@ -176,7 +177,7 @@ const App = () => {
                             );
                         })
                     ) : (
-                        <div className="text-gray-500 text-center py-24 ">
+                        <div className="py-24 text-center text-gray-500 ">
                             Please select a crop to view recommendations
                         </div>
                     )}
